5b7b0321a69d432384a1d97aca6e2ebd223548f2
naveenprabhu353-droid <naveenprabhu353@gmail.com>
2025-08-08 10:49:53 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,44 +1,24 @@
-from flask import Flask, render_template,request
+from flask import Flask
+import mysql.connector
+app=Flask(__name__)
 
-app = Flask(__name__)
-
-# Home page route
 @app.route('/')
-def home():
-    return render_template('home.html')
-
-@app.route('/course')
-def cours():
-    return render_template("course.html")
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-@app.route("/register")
-def sign():
-    return render_template("register.html")
-
-@app.route("/register",methods=["GET","POST"])
-def register():
-    if request.method=='POST':
-        name=request.form["name"]#Error
-        password=request.form.get("password") #None
-        confirm_password=request.form.get("confirm_password")
-        email=request.form.get("email")
-    
-    if not name or not password or not confirm_password or not email:
-        return "❌ All Values are Required"
-    if len(name)<=3:
-        return "❌ Your Name must be 3 characters"
-    if len(password)<=3:
-        return "❌ The Password is must be 4 values"
-    if password!=confirm_password:
-        return "❌ The Password is mismatch"
-    if "@" not in email or "." not in email:
-        return "❌ Enter Valid Email"
-    return render_template("login.html",name=name,password=password)
+def create_table():
+    conn=mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='root',
+        database='hrr'
+    )
+    cur=conn.cursor()
+    cur.execute("create table if not exists kevin(id int auto_increment primary key,name varchar(50),mark int)")
+    conn.commit()
+    conn.close()
+    return "table created successfully"
 
 
-if __name__ == '__main__':
+
+
+
+if __name__=='__main__':
     app.run(debug=True)
